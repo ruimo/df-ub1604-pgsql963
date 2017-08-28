c0fb1e0c1226908bd1bdb150693017dc21462ede
花井志生 <shanai@hanaishisei-no-MacBook-Pro-2.local>
2017-08-28 19:55:42 +0900

    for mac

---
+++ init-mac.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Run this script at the first time you launch pgsql image.
+docker run -u 0 --rm -v $HOME:/var/home:rw -v /private/var/pg_db:/var/pgsql:rw ruimo/df-ub1604-pgsql96x /initdb.sh
---
+++ launch-mac.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run -d -p 5432:5432 -v /private/var/pg_db:/var/pgsql:rw -v /etc/localtime:/etc/localtime:ro --name postgres-db ruimo/df-ub1604-pgsql96x $*
